0d0c0400e118fbb54ae578baf3775f7bb2d683f6
Maksym Sobol <55525227+sobol-mo@users.noreply.github.com>
2022-12-15 16:16:24 +0200

    1rst progress function correction

--- pom/stochastic03/utils/progress.py
+++ pom/stochastic03/utils/progress.py
@@ -1,12 +1,25 @@
+"""
+This module includes a function that displays progress bar in console.
+Based on:
+https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+"""
+
 import sys
 
 
 def progress(count, total, suffix=''):
+    """
+    Call in a loop to create terminal progress bar.
+    :param count:   - Required: current iteration (Int)
+    :param total:   - Required: total iterations (Int)
+    :param suffix:  - Optional: arbitrary suffix (default '')
+    :return:
+    """
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
     percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    bar_ = '=' * filled_len + '-' * (bar_len - filled_len)
 
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+    sys.stdout.write(f'\r[{bar_}] {percents}% {suffix}')
     sys.stdout.flush()  # As suggested by Rom Ruben

9d029e0e446956dda3fd94d8afd51d3480d4b556
Maksym Sobol <55525227+sobol-mo@users.noreply.github.com>
2023-05-02 19:04:38 +0300

    Skeleton created

--- pom/paper01/InitData/inizialize_data.py
+++ pom/paper01/InitData/inizialize_data.py
@@ -2,6 +2,40 @@
 initial data
 """
 experiments = {
+    "0002": {
+        "file_name": "deterministic_flow_harmonics"
+        , "tau_correlation": 0.025
+
+        , "relative_sigma_correlation": 1.0
+
+        , "period": 200
+        , "c0_show_var2": {"0": 0, "1": 1, "2": 1, "3": 1, "4": 1, "5": 1}
+        , "quality_criterion": {"0": 0, "1": 1, "2": 2, "3": 1, "4": 1, "5": 1}
+        , "show_prepare_k": True
+        , "number_of_harmonics": 50
+        , "number_of_intervals_xi2": 50
+        , "plot_parameters": {
+            "dpi": 1000
+            , "deterministic_flow_harmonics": {
+                "visual_line_set": {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5}
+                , "fontsize": 9
+                , "alpha_main": 1.0
+                , "alpha_grid": 1.0
+                , "color": "black"
+                , "x_tick_main": 0.1
+                , "x_tick_auxiliary": 0.05
+                , "x_axis_order": "forward"  # "back" | "forward"
+                , "y_tick_main": 0.4
+                , "y_tick_auxiliary": 0.2
+                , "x_label_name": r'$\tau$'
+                , "y_label_name": r'$\gamma_d$($\tau$)'
+                , "x_size_plot": 86.5  # 172.8 # 86.5
+                , "y_size_plot": 72.0  # 130.0 #65.0
+                , "plot_line_width": 2.0
+                , "grid_line_width": 1.0
+            }
+        }
+    },
     "0001": {
         "file_name": "EngineMechanicalCharacteristics"
         , "tau_correlation": 0.025
@@ -19,20 +53,20 @@ experiments = {
             , "engine_mechanical_characteristics": {
                 "visual_line_set": {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5}
                 , "fontsize": 9
-                , "alpha_main" : 1.0
-                , "alpha_grid" : 1.0
-                , "color" : "black"
-                , "x_tick_main" : 0.1
-                , "x_tick_auxiliary" : 0.05
-                , "x_axis_order" : "forward"      # "back" | "forward"
-                , "y_tick_main" : 0.4
-                , "y_tick_auxiliary" : 0.2
-                , "x_label_name" : r'$\tau$'
-                , "y_label_name" : r'$\gamma_d$($\tau$)'
-                , "x_size_plot" : 86.5 #172.8 # 86.5
-                , "y_size_plot" : 72.0 #130.0 #65.0
-                , "plot_line_width" : 2.0
-                , "grid_line_width" : 1.0
+                , "alpha_main": 1.0
+                , "alpha_grid": 1.0
+                , "color": "black"
+                , "x_tick_main": 0.1
+                , "x_tick_auxiliary": 0.05
+                , "x_axis_order": "forward"  # "back" | "forward"
+                , "y_tick_main": 0.4
+                , "y_tick_auxiliary": 0.2
+                , "x_label_name": r'$\tau$'
+                , "y_label_name": r'$\gamma_d$($\tau$)'
+                , "x_size_plot": 86.5  # 172.8 # 86.5
+                , "y_size_plot": 72.0  # 130.0 #65.0
+                , "plot_line_width": 2.0
+                , "grid_line_width": 1.0
             }
         }
     }
---
+++ pom/paper01/dflow/d_flow_analysis.py
@@ -0,0 +1,82 @@
+"""Creating plots of harmonics to analyze the deterministic part of input flow
+"""
+
+import math
+import pandas as pd
+
+import pom.stochastic03.utils.FileUtil as file_util
+import pom.stochastic03.Graphics.LineCharts.LineChart as line_chart
+
+
+class DeterministicFlowHarmonics:
+    """
+    Visualization of the deterministic flow harmonics
+    :param experiment: experiment conditions.
+    """
+
+    def __init__(self, experiment):
+        self.experiment = experiment
+        self.result_data = 'resultData/'
+        self.sub_directory_name = ''
+        self.file_name_prefix = 'd_flow_harm_'
+        self.size = 1000
+        self.plot_parameters = 'plot_parameters'
+        self.plot_name = 'deterministic_flow_harmonics'
+
+    def show(self):
+        path = self.result_data + self.experiment['file_name'] + self.sub_directory_name
+        file_util.make_dir_if_not(path)
+
+        lines = pd.DataFrame()
+        temp_size = int(self.size)
+        lines['tau'] = [0.0] * round(temp_size)
+        lines['harmonic0'] = [0.0] * round(temp_size)
+        lines['harmonic1'] = [0.0] * round(temp_size)
+        lines['harmonic2'] = [0.0] * round(temp_size)
+        lines['harmonic3'] = [0.0] * round(temp_size)
+        lines['harmonic4'] = [0.0] * round(temp_size)
+
+        for i in range(temp_size):
+            tau = float(i) / self.size
+            lines['tau'][i] = tau
+            lines['harmonic0'][i] = math.cos(2 * math.pi * tau)
+            lines['harmonic1'][i] = 2.0 * math.cos(3 * math.pi * tau)
+            lines['harmonic2'][i] = math.cos(4 * math.pi * tau)
+            lines['harmonic3'][i] = math.cos(5 * math.pi * tau)
+            lines['harmonic4'][i] = math.cos(5 * math.pi * tau)
+
+        plot_values = [lines['tau'].values
+            , lines['harmonic0'].values
+            , lines['harmonic1'].values
+            , lines['harmonic2'].values
+            , lines['harmonic3'].values
+            , lines['harmonic4'].values
+                       ]
+
+        x_values = plot_values[0]
+        y_values = line_chart.visual_lines(plot_values, self.experiment, self.plot_name)
+        params = self.experiment[self.plot_parameters][self.plot_name]
+        line_chart.line_plot3(path + '/' + self.file_name_prefix
+                              , x_values
+                              , y_values
+                              , xlabel_name=params['x_label_name']
+                              , title=params['y_label_name']
+                              , _alpha_main=params['alpha_main']
+                              , _alpha_grid=params['alpha_grid']
+                              , _color=params['color']
+                              , _dpi=self.experiment[self.plot_parameters]['dpi']
+                              , x_min=min(x_values)
+                              , x_max=1
+                              , x_tick_main=params['x_tick_main']
+                              , x_tick_auxiliary=params['x_tick_auxiliary']
+                              , x_axis_order=params['x_axis_order']
+                              , y1_min=-2
+                              , y1_max=2
+                              , y_tick_main=params['y_tick_main']
+                              , y_tick_auxiliary=params['y_tick_auxiliary']
+                              , _fontsize=params['fontsize']
+                              , _x_size_plot=params['x_size_plot']
+                              , _y_size_plot=params['y_size_plot']
+                              , _plot_line_width=params['plot_line_width']
+                              , _grid_line_width=params['grid_line_width']
+                              )
